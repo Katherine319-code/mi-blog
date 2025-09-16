854c73b3f4d99c47e476190ec79ab1e96e40ff31
Katherine319-code <hlkatherine319@gmail.com>
2025-09-16 16:56:30 -0500

    Sistema 2  de comentarios permanentes con backup en GitHub

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const cors = require('cors');
-const db = require('./database-json');
+const db = require('./database-github');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
