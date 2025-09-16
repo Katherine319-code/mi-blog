d513e210e2e4c094de9cad8f6fb7a90b0a2d1ef6
Katherine319-code <hlkatherine319@gmail.com>
2025-09-16 14:59:46 -0500

    Reemplazado SQLite por JSON para Render

--- server/server.js
+++ server/server.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const path = require('path');
 const cors = require('cors');
-const db = require('./database');
-
+const db = require('./database-json');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
